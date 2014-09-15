4175b3feb71bbeb404fda17e0ebb042212d990e8
Matt Fichman <matt.fichman@gmail.com>
2014-09-15 08:00:35 -0400

    Added atomic block

---
+++ include/binfmt/AtomicBlock.hpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2014 Matt Fichman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, APEXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "binfmt/Buffer.hpp"
+
+namespace binfmt {
+
+class AtomicBlock {
+// Asserts that read/write operations in the current scope are atomic; resets
+// the buffer back to the original state if the whole atomic block can't be
+// serialized or deserialized in the given buffer space.
+public:
+    enum Mode { READ, WRITE };
+    AtomicBlock(Buffer* buffer, Mode mode);
+
+    bool commit();
+
+private:
+    Buffer* buffer_;
+    Mode mode_;
+    size_t errors_;
+    size_t limit_;
+    size_t position_;
+};
+
+}
---
+++ src/AtomicBlock.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2014 Matt Fichman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, APEXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once 
+
+#include "binfmt/Common.hpp"
+#include "binfmt/AtomicBlock.hpp"
+
+using namespace binfmt;
+
+AtomicBlock::AtomicBlock(Buffer* buffer, Mode mode) : buffer_(buffer), mode_(mode) {
+    errors_ = buffer_->errors();
+    limit_ = buffer_->limit();
+    position_ = buffer_->position();
+}
+
+bool AtomicBlock::commit() {
+    switch (mode_) {
+    case READ:
+        assert(buffer_->limit()==limit_); // FIXME: Add a better check for writes
+        break;
+    case WRITE:
+        assert(buffer_->position()==position_); // FIXME: Add a better check for reads
+        break;
+    default:
+        assert(!"unknown mode");
+        break;
+    }
+    if (buffer_->errors() != errors_) {
+        buffer_->positionIs(buffer_->position()); 
+        buffer_->limitIs(buffer_->limit()); 
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
